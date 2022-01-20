16944dadead2018c836f30215d17e8e28a22567b
Trevor Price <trevor.price@perficient.com>
2022-01-20 11:35:19 -0600

    chore: Setup Gradle
    Create new gradle project

---
+++ src/main/java/com/perficient/techbootcampeCommerce/TechbootcampECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampeCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perficient/techbootcampeCommerce/TechbootcampECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampeCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbootcampECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
